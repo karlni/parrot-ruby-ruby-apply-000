0dadb9a3b565e572c1ed34d8e6713d07f7df5581
agent.karl.zl.ni@gmail.com <agent.karl.zl.ni@gmail.com>
2018-10-16 18:58:43 +0000

    automatically backed up by learn

--- parrot.rb
+++ parrot.rb
@@ -1,6 +1,6 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
-def parrot ("a", "Squawk!")
-  print parrot ("a")
+def parrot (string = "Squawk!")
+  print "{string}"  "{string}"
 end
   
